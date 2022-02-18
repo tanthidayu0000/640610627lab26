09d0bbfa338cde84e950003c3e07b21c9fb4e724
thidayu <tanthidayu_0000@hotmail.com>
2022-02-18 16:14:42 +0700

    lab26.3 finished

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
+#include <set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[], int idx){
+	int count=0, s=0;
+	set<int> mySet;
+	for(int i=0; i<idx; i++){
+		s=0;
+		for(int j=i; j<idx; j++){
+			if(data[j]==data[i]){
+				s++;
+			}
+
+		}
+		if(s==1){
+			mySet.insert(data[i]);
+			count++;
+		}
+	}
+	return count;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
